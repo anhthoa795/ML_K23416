5905ff24b28e765733000248b780268f5f66a561
anhthoa795 <thoahta23406@st.uel.edu.vn>
2025-09-24 14:42:56 +0700

    Delete 24thang9_K234060729 directory

--- 24thang9_K234060729/Demo_Xml_BeautifulSoup.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-with open('../datasets/SalesTransactions/SalesTransactions.xml','r') as f:
-    data = f.read()
-
-bs_data = BeautifulSoup(data,'xml')
-UelSample = bs_data.find_all('UelSample')
-print(UelSample)
